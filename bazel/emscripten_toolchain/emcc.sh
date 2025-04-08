cd4314e4151b480216e84d96c9d902a6e811ca83
James Judd <james@lab-y.com>
2025-04-07 20:52:48 -0600

    Hacky workaround for path mapping params file issue

--- bazel/emscripten_toolchain/emcc.sh
+++ bazel/emscripten_toolchain/emcc.sh
@@ -2,4 +2,14 @@
 
 source $(dirname $0)/env.sh
 
-exec $EMSDK_PYTHON $EMSCRIPTEN/emcc.py "$@"
+# Workaround for what I think is a path mapping bug: hackily path map the file
+# if it isn't path mapped.
+# For some reason the params file isn't being path mapped and I don't know if that
+# is a rule set issue or a bazel issue yet.
+if [ -e $(echo "$@" | sed s/@//g) ]; then
+    args="$@"
+else
+    args=$(echo "$@" | sed "s=bazel-out/.*/bin=bazel-out/cfg/bin=g")
+fi
+
+exec $EMSDK_PYTHON $EMSCRIPTEN/emcc.py ${args}
